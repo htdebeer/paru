554b1872106284bf1d2cde05b9b66470dd7d54ad
Huub de Beer <Huub@heerdebeer.org>
2017-06-14 09:23:14 +0200

    Removed special metadata manipulation/inspection methods: from now on the metadata is just a Ruby Hash and you can work with it accordingly.

--- test/test_metadata_filter.rb
+++ test/test_metadata_filter.rb
@@ -11,10 +11,6 @@ class MetadataFilterTest < FilterTest
             metadata["date"] = "#{Date.today.to_s}"
         end
         assert_match(/#{Date.today.to_s}/, output)
-        output = filter_file("test/pandoc_input/add_today.md") do
-            metadata.set_by "date", "#{Date.today.to_s}"
-        end
-        assert_match(/#{Date.today.to_s}/, output)
     end
 
     def test_simple_metadata()
@@ -25,15 +21,6 @@ class MetadataFilterTest < FilterTest
             metadata['title'] = "Say hello to the world"
             metadata['date'] = "12-12-1812"
             metadata['author'] = "Huub de Beer"
-        end
-        
-        filter_file_and_equal_file(
-            "test/pandoc_input/hello.md",
-            "test/pandoc_output/hello.md"
-        ) do
-            metadata.set_by 'title', "Say hello to the world"
-            metadata.set_by 'date', "12-12-1812"
-            metadata.set_by 'author', "Huub de Beer"
         end
     end
 
@@ -49,18 +36,6 @@ class MetadataFilterTest < FilterTest
             has_title = metadata.has_key? "title"
         end
         assert(has_title == false)
-        
-        has_title = true
-        filter_file_and_equal_file(
-            "test/pandoc_input/procent_based_metadata.md",
-            "test/pandoc_output/procent_based_metadata_cleared.md"
-        ) do
-            metadata.delete_by("date")
-            metadata.delete_by("author")
-            metadata.delete_by("title")
-            has_title = metadata.has_by? "title"
-        end
-        assert(has_title == false)
     end
 
     def test_add_subtitle()
@@ -72,16 +47,6 @@ class MetadataFilterTest < FilterTest
         ) do
             metadata["subtitle"] = subtitle
             has_subtitle = metadata.has_key? "subtitle"
-        end
-        assert(has_subtitle)
-
-        has_subtitle = false
-        filter_file_and_equal_file(
-            "test/pandoc_input/bold_code.md",
-            "test/pandoc_output/bold_code_with_subtitle.md"
-        ) do
-            metadata.set_by "subtitle", subtitle
-            has_subtitle = metadata.has_by? "subtitle"
         end
         assert(has_subtitle)
     end
@@ -114,28 +79,6 @@ class MetadataFilterTest < FilterTest
             "test/pandoc_output/bold_code_sub_property3.md"
         ) do
             metadata["property"] = {"sub2" => "value"}
-        end
-        
-        filter_file_and_equal_file(
-            "test/pandoc_input/bold_code.md",
-            "test/pandoc_output/bold_code_sub_property.md"
-        ) do
-            if not metadata.has_by? "property"
-                metadata["property"] = {}
-            end
-
-            metadata.set_by "property.sub", "value"
-        end
-
-        filter_file_and_equal_file(
-            "test/pandoc_output/bold_code_sub_property.md",
-            "test/pandoc_output/bold_code_sub_property2.md"
-        ) do
-            if not metadata.has_key? "property"
-                metadata["property"] = {}
-            end
-
-            metadata.set_by "property.sub2", "value"
         end
     end
 end
